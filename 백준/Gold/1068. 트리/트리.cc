31f20fd2a910a3c2968ff4b2dc3b7009cd36de7a
unistuj <115923641+unistuj@users.noreply.github.com>
2023-08-09 22:10:38 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1068. 트리/트리.cc
+++ 백준/Gold/1068. 트리/트리.cc
@@ -1,30 +1,43 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
 using namespace std;
-int n, r, temp, root;
-vector<int> adj[54];
-int dfs(int here){
-    int ret = 0;
-    int child = 0;
-    for(int there : adj[here]){
-        if(there == r) continue;
-        ret += dfs(there);
-        child++;
-    }
-    if(child == 0) return 1;
-    return ret;
+
+int n,e, node;
+int cnt;
+vector <int> no[51];
+
+void countnode(int p) {
+	int child = 0;
+	for (int i = 0; i <no[p].size(); i++) {
+		int t = no[p][i];
+		if(t == e)continue;
+		child++;
+		countnode(t);
+	}
+	if(child == 0)cnt++;
 }
-int main(){
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for(int i = 0; i < n; i++){
-        cin >> temp;
-        if(temp == -1)root = i;
-        else adj[temp].push_back(i);
-    }
-    cin >> r;
-    if(r == root){
-        cout << 0 << "\n";return 0;
-    }
-    cout << dfs(root) << "\n";
-    return 0;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		int temp;
+		cin >> temp;
+		if(temp == -1)node = i;
+		else{
+			no[temp].push_back(i);
+		}
+	}
+	cin >> e;
+	
+	if(e == node){
+		cout << 0 << "\n";
+		return 0;
+	}else{
+		countnode(node);
+		cout << cnt << "\n";
+	}
+	
 }
