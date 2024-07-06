f240df14fc23396e9d7681edaabf3dc972c5bf5e
unistuj <115923641+unistuj@users.noreply.github.com>
2024-07-06 21:40:34 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -0,0 +1,8 @@
+-- 코드를 작성해주세요
+
+select distinct i.id, i.email, i.first_name, i.last_name
+from developers i cross join (select code
+                   from skillcodes
+                   where category= 'Front End') c
+where (conv(i.skill_code, 10, 2) - conv(c.code, 10, 2)) not like '%9%'
+order by id
